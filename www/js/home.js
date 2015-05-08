79e641f14dbfa269c585c2c80da0de8e06d95bcc
Kelvin Tamzil <kelvintamzil@yahoo.com>
2015-05-08 15:38:07 +1000

    change: user data

--- www/js/home.js
+++ www/js/home.js
@@ -2,7 +2,6 @@ angular.module('starter')
 
 .controller('HomeCtrl', function($scope, $ionicModal, $state, $rootScope) {
     $scope.user = JSON.parse(window.localStorage.userID);
-
     $scope.startWalking = function () {
     	$state.go('sideMenu.inwalk');
     }
--- www/js/login.js
+++ www/js/login.js
@@ -2,31 +2,40 @@ angular.module('starter')
 
 .controller('LoginCtrl', function($scope, $ionicModal, $state, $rootScope) {
     // Form data for the login modal
-    var loginData = {
+    var loginData = [{
         user: 'admin',
-        password: 'admin'
-    };
+        password: 'admin',
+        weight: 60,
+        height: 170
+    }];
     $scope.enteredLogin = {};
     $scope.wrongPassword = false;
     console.log(window.localStorage);
     if (window.localStorage.userID) {
-      $state.go('sideMenu.home');
+        $state.go('sideMenu.home');
     }
     // Perform the login action when the user submits the login form
     $scope.doLogin = function() {
-        console.log($state);
-        if ($scope.enteredLogin.username === loginData.user && $scope.enteredLogin.password === loginData.password) {
+        var userDetails = _.find(loginData, function(data) {
+            return data.user === $scope.enteredLogin.username && data.password === $scope.enteredLogin.password;
+        });
+        console.log('user is', userDetails);
+        if (userDetails) {
+            //rootscope doesnt work? I wonder why
             $rootScope.userAccount = {
-              username: $scope.enteredLogin.username,
-              password: $scope.enteredLogin.password
+                username: $scope.enteredLogin.username,
+                password: $scope.enteredLogin.password
             };
             console.log('user correct');
-            window.localStorage.userID = JSON.stringify($scope.enteredLogin);
-            console.log($rootScope);
-            $state.go('sideMenu.home', null, {reload: true});
+
+            window.localStorage.userID = JSON.stringify(userDetails);
+            $state.go('sideMenu.home', null, {
+                reload: true
+            });
 
         } else {
             $scope.wrongPassword = true;
+            console.log('login not found');
         }
     };
 });
