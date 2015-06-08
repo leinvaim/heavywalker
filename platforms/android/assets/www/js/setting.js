4fec31a29026e388aef6b2936c5f2aa8d47631b9
Kelvin Tamzil <kelvintamzil@yahoo.com>
2015-06-08 18:37:28 +1000

    remove: platforms and plugins

--- platforms/android/assets/www/js/setting.js
+++
@@ -1,6 +0,0 @@
-angular.module('starter')
-
-.controller('SettingCtrl', function($scope, $ionicModal, $state, $rootScope) {
-    $scope.user = JSON.parse(window.localStorage.userID);
-
-});
--- platforms/ios/www/js/profile.js
+++
@@ -1,6 +0,0 @@
-angular.module('starter')
-
-.controller('ProfileCtrl', function($scope, $ionicModal, $state, $rootScope) {
-    $scope.user = JSON.parse(window.localStorage.userID);
-
-});
